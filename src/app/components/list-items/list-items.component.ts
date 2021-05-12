840c1ac82ca1a3dce3fc857765ff26e190aa4a52
Niall Eccles <nialle@rothco.ie>
2021-05-12 21:57:27 +0100

    Added active class.

--- src/app/components/list-items/list-items.component.ts
+++ src/app/components/list-items/list-items.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-list-items',
-  templateUrl: './list-items.component.html',
-  styleUrls: ['./list-items.component.scss']
+  selector: "app-list-items",
+  templateUrl: "./list-items.component.html",
+  styleUrls: ["./list-items.component.scss"],
 })
 export class ListItemsComponent implements OnInit {
+  public activeItemId: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.activeItemId = 0;
   }
 
+  ngOnInit(): void {}
+
+  public setActiveItemId(id: number) {
+    this.activeItemId = id;
+  }
 }
